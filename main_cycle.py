ad0f5e241d0caa317f4db0255b9e9d299a6225a4
Владимир <ferrovovan@mail.ru>
2021-12-19 15:40:13 +0500

    Добавлен стартовый экран, в соответсвии с учебником.

--- main_cycle.py
+++ main_cycle.py
@@ -1,38 +1,50 @@
 import pygame
 from Classis import Map, Hero
+from menu import start_screen
 
 pygame.init()
 pygame.display.set_caption('Поход по горам')
 a = 900
 size = a, a
 screen = pygame.display.set_mode(size)
-board = Map(16, 16, screenBoards=size)
-board.load_map('data/maps/main_map.txt', size)
-hero1 = Hero(board.board, 0, 7, is_in_circle=True)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            kPressed = pygame.key.get_pressed()  # нажатые кнопки
-            if kPressed[pygame.K_ESCAPE]:
+FPS = 60
+
+
+def main():
+    board = Map(16, 16, screenBoards=size)
+    board.load_map('data/maps/main_map.txt', size)
+    hero1 = Hero(board.board, 0, 7, is_in_circle=True)
+    # основной цикл
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
-            if kPressed[pygame.K_UP]:
-                board.move(y=-10)
-            if kPressed[pygame.K_DOWN]:
-                board.move(y=10)
-            if kPressed[pygame.K_RIGHT]:
-                board.move(x=10)
-            if kPressed[pygame.K_LEFT]:
-                board.move(x=-10)
-            if kPressed[pygame.K_w]:
-                hero1.move((1, 0))
-            elif kPressed[pygame.K_s]:
-                hero1.move((-1, 0))
-        if event.type == pygame.MOUSEWHEEL:  # если мышь крутится
-            board.zoom(event.y, screen.get_size())
-    screen.fill('black')
-    board.render(screen)
-    pygame.display.flip()
-pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                kPressed = pygame.key.get_pressed()  # нажатые кнопки
+                if kPressed[pygame.K_ESCAPE]:
+                    running = False
+                if kPressed[pygame.K_UP]:
+                    board.move(y=-10)
+                if kPressed[pygame.K_DOWN]:
+                    board.move(y=10)
+                if kPressed[pygame.K_RIGHT]:
+                    board.move(x=10)
+                if kPressed[pygame.K_LEFT]:
+                    board.move(x=-10)
+                if kPressed[pygame.K_w]:
+                    hero1.move((1, 0))
+                elif kPressed[pygame.K_s]:
+                    hero1.move((-1, 0))
+            if event.type == pygame.MOUSEWHEEL:  # если мышь крутится
+                board.zoom(event.y, screen.get_size())
+        screen.fill('black')
+        board.render(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
+
+
+if start_screen(screen, FPS):
+    main()
---
+++ menu.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+def start_screen(screen, FPS):
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    screen.fill('blue')
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    clock = pygame.time.Clock()
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return True
+        pygame.display.flip()
+        clock.tick(FPS)
