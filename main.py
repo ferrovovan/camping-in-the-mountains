ed0e4ebb05aa41b6c86f5dfcf10e565b18fb4561
Владимир <ferrovovan@mail.ru>
2021-12-23 19:01:19 +0500

    main_cycle.py и main_menu.py объеденились в main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pygame
-from Classis import Map, Hero, Interface, MouseManager, Character
-from main_menu import start_screen
+from Classis import *
 
 pygame.init()
 pygame.display.set_caption('Поход по горам')
@@ -19,6 +17,52 @@ for key in settingsDict.keys():
         size = tuple(map(int, settingsDict[key].split(', ')))
 
 screen = pygame.display.set_mode(size)
+
+
+def start_screen(screen, FPS):
+    screen.fill([255, 255, 255])
+    menuIm = load_image('gfx/textures/interface/fone.png')
+    screen.blit(menuIm, menuIm.get_rect())
+    clock = pygame.time.Clock()
+
+    images = {'button1': load_image('gfx/buttons/button1.png')}
+
+    button_sprites = ButtonGroup()
+    scr_size = screen.get_size()
+    butt_width = 300
+    butt_height = 100
+    x = [4, 3, 6, 1]
+    n = 4
+    for i in range(1, n + 1):
+        Button(button_sprites, scr_size[0] // 2 - butt_width // 2,
+               scr_size[1] // 2 + int((n // 2 - i) * butt_height * 1.2) + int(butt_height * 3 / n),
+               butt_width, butt_height, id=x[i - 1], image=images['button1'])
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                id = button_sprites.click_id(event)
+                if id is not None:
+                    # готово
+                    if id == 1:
+                        return True
+                    elif id == 3:
+                        pass
+                    # готово
+                    elif id == 4:
+                        pygame.quit()
+                        exit()
+                    elif id == 6:
+                        pass
+
+        button_sprites.draw(screen)
+        button_sprites.draw_text(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 def main():
