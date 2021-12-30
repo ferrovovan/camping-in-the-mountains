f7a83b68f0ac9e4ea7838e53bce0da47f35a2666
Владимир <ferrovovan@mail.ru>
2021-12-30 18:04:42 +0500

    Перед # поставил _

--- Fight_sistem.py
+++ Fight_sistem.py
@@ -44,7 +44,7 @@ def main():
                 return False
             if event.type == pygame.MOUSEBUTTONDOWN and not end:
                 mouse_pos = event.pos
-                #кнопка атаки
+                # кнопка атаки
                 if button_attack.collidepoint(mouse_pos) and not Action:
                     mercy = 0
                     words = '* Банда ждёт ваших действий!'
@@ -62,7 +62,7 @@ def main():
                             XP_Hero -= Band_hit + dop_attack
                             XP_band -= Your_hit
                     dop_attack = 0
-                #кнопка действия
+                # кнопка действия
                 elif button_action.collidepoint(mouse_pos) and not Action:
                     if XP_Hero - (Band_hit + dop_attack) <= 0:
                         XP_Hero = 0
@@ -73,7 +73,7 @@ def main():
                         words = ''
                     dop_attack = 0
                     Action = True
-                #кнопка предмета
+                # кнопка предмета
                 elif button_item.collidepoint(mouse_pos) and not Action and item_col < 5:
                     item_col += 1
                     words = '* Банда ждёт ваших действий!'
@@ -90,7 +90,7 @@ def main():
                     if XP_band < 120:
                         XP_band += 1
                     dop_attack = 0
-                #кнопка пощады
+                # кнопка пощады
                 elif button_mercy.collidepoint(mouse_pos) and not Action:
                     if mercy >= 4:
                         end = True
@@ -109,7 +109,7 @@ def main():
                         screen.blit(text, textRect)
                         pygame.draw.rect(screen, 'white', Action_rect, 1)
                         Band_talk = 'None'
-                #кнопка угрозы
+                # кнопка угрозы
                 elif button_threat.collidepoint(mouse_pos) and Action:
                     mercy = 0
                     chance = randint(0, 100)
@@ -118,7 +118,7 @@ def main():
                     else:
                         Band_scare = 'refuse'
                     Action = False
-                #кнопка разговора
+                # кнопка разговора
                 elif button_talk.collidepoint(mouse_pos) and Action:
                     chance = randint(0, 100)
                     if chance >= 20:
@@ -127,16 +127,16 @@ def main():
                         Band_talk = 'refuse'
                     Action = False
         screen.fill('black')
-        #механника действий
+        # механника действий
         if Action and not end:
-            #button_threat = pygame.Rect(80, 500, 120, 100)
+            # button_threat = pygame.Rect(80, 500, 120, 100)
             text = font.render('* Угрожать', True, 'white')
             textRect = text.get_rect()
             textRect.center = button_threat.center
             screen.blit(text, textRect)
             pygame.draw.rect(screen, 'black', button_threat, 1)
 
-            #button_talk = pygame.Rect(650, 500, 120, 100)
+            # button_talk = pygame.Rect(650, 500, 120, 100)
             text = font.render('* Поговорить', True, 'white')
             textRect = text.get_rect()
             textRect.center = button_talk.center
@@ -149,7 +149,7 @@ def main():
             screen.blit(text, textRect)
             pygame.draw.rect(screen, 'white', Action_rect, 1)
         else:
-            #угрожать
+            # угрожать
             if Band_scare == 'good':
                 dop_attack = -10
                 words = 'Банда в испуге! -10 к атаке банды'
@@ -168,7 +168,7 @@ def main():
                 screen.blit(text, textRect)
                 pygame.draw.rect(screen, 'white', Action_rect, 1)
                 Band_scare = 'None'
-            #поговорить
+            # поговорить
             elif Band_talk == 'refuse':
                 dop_attack = 5
                 words = 'Банда не хочет с вами говорить. +5 к атаке банды'
